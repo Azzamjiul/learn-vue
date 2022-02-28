9fed0391197ecbc2d2094caa0f4fe18b1a586761
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-02-28 17:28:36 +0700

    define and use method

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             title: "Comedy Events",
-            tickets: 0,
+            tickets: 3,
             image: "http://www.fillmurray.com/200/200",
             details: ["Funny comics", "Mime artistry", "Satire", "Sketches"],
             cart: 0,
         }
-    }
+    },
+    methods: {
+        updateCart(number = 1) {
+            this.cart += number
+            this.tickets -= number
+        }
+    },
 })

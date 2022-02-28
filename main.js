712989199e736db50214e0bd1e7a982493d4cedd
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-02-28 17:49:23 +0700

    mouseover

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ const app = Vue.createApp({
             image: "http://www.fillmurray.com/200/200",
             details: ["Funny comics", "Mime artistry", "Satire", "Sketches"],
             cart: 0,
+            ticketTypes : [
+                { id: "bronzeTicket", name: "Bronze", image: "http://www.fillmurray.com/198/198", tickets: 3, descriptions: "This is a bronze level fun" },
+                { id: "silverTicket", name: "Silver", image: "http://www.fillmurray.com/202/202", tickets: 10, descriptions: "This is some silver level fun" },
+                { id: "goldTicket", name: "Gold", image: "http://www.fillmurray.com/199/199", tickets: 40, descriptions: "Go all in - this is teh gold level!" },
+            ]
         }
     },
     methods: {
         updateCart(number = 1) {
             this.cart += number
             this.tickets -= number
+        },
+        updateImage(id) {
+            this.image = this.ticketTypes.filter(type => type.id === id)[0].image
         }
     },
 })
